5592540a30b91c3a35b5f4645d44ed604241c643
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2026-01-06 09:20:49 +0300

    Update change-mode.sh

--- change-mode.sh
+++ change-mode.sh
@@ -3,7 +3,6 @@ cd "$(dirname "$0")"
 lsof -t ./tor/ld-linux-x86-64.so.2 | xargs kill
 systemctl --user disable tor.service --now
 rm ~/.config/systemd/user/tor.service
-PS3='Welcome to the mode control panel. Do you want to set the mode to pro, set the mode to default, or remove middle nodes?'
 options=("Change the mode to pro" "Change the mode to default" "Remove middle nodes")
 select opt in "${options[@]}"
 do
