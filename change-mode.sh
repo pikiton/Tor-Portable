2ccfed27cf65fe0f08fb88663c9e7761c73c31c3
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2026-01-06 09:41:22 +0300

    Update change-mode.sh

--- change-mode.sh
+++ change-mode.sh
@@ -5,14 +5,17 @@ systemctl --user disable tor.service --now
 rm ~/.config/systemd/user/tor.service
 read -n 1 -p "Welcome to the mode control panel. Do you want to set the mode to pro (1), set the mode to default (2), or remove middle nodes (3)?" INP
 if [[ $INP = 1 ]]; then
+echo
 cp ./change-mode/pro/torrc.txt torrc.txt
 read -p "The mode was changed to pro."
 fi
 if [[ $INP = 2 ]]; then
+echo
 cp ./change-mode/default/torrc.txt torrc.txt
 read -p "The mode was changed to default."
 fi
 if [[ $INP = 3 ]]; then
+echo
 sed -i 's/MiddleNodes/#MiddleNodes/' torrc.txt
 read -p "Middle nodes were removed."
 fi
