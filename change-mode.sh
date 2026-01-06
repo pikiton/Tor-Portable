940049863f10b3db5cd4757d1719289ad6740366
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2026-01-06 09:00:09 +0300

    Update change-mode.sh

--- change-mode.sh
+++ change-mode.sh
@@ -3,17 +3,22 @@ cd "$(dirname "$0")"
 lsof -t ./tor/ld-linux-x86-64.so.2 | xargs kill
 systemctl --user disable tor.service --now
 rm ~/.config/systemd/user/tor.service
-if grep -q "The mode is default" torrc.txt; then
-cp ./change-mode/pro/torrc.txt torrc.txt
-read -p "The mode was changed to pro."
-exit
-fi
-
-if grep -q "The mode is pro" torrc.txt; then
-cp ./change-mode/default/torrc.txt torrc.txt
-read -p "The mode was changed to default."
-exit
-else
-cp ./change-mode/default/torrc.txt torrc.txt
-read -p "An error occurred. Attempted to update the mode and set it to default. The error should be gone."
-fi
+PS3='Welcome to the mode control panel. Do you want to set the mode to pro, set the mode to default, or remove middle nodes?'
+options=("Change the mode to pro" "Change the mode to default" "Remove middle nodes")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Change the mode to pro")
+            cp ./change-mode/pro/torrc.txt torrc.txt
+            read -p "The mode was changed to pro."
+            ;;
+        "Change the mode to default")
+            cp ./change-mode/default/torrc.txt torrc.txt
+            read -p "The mode was changed to default."
+            ;;
+        "Remove middle nodes")
+            sed 's/MiddleNodes/#MiddleNodes/' torrc.txt
+            read -p "Middle nodes were removed"
+            ;;
+    esac
+done
