e7008b22e8b567438704bcc2b5633b8107ae70d3
Chara-Freedom <lolered@yandex.ru>
2026-01-21 07:29:27 +0300

    Add files via upload
    Continue rewriting, getting VERSION

--- AntiTor.cpp
+++ AntiTor.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <windows.h>
 #include <filesystem>
 int main() {
@@ -5,6 +6,11 @@ WCHAR path[MAX_PATH];
 GetModuleFileNameW(NULL, path, MAX_PATH);
 std::filesystem::path fullpath(path);
 std::filesystem::current_path(fullpath.remove_filename());
+FILE* pipe = _popen("@echo off & for %I in (VERSION*) do echo %~nxI", "r");
+char buffer[100];
+std::string result;
+while (fgets(buffer, sizeof(buffer), pipe) != NULL) result += buffer;
+_pclose(pipe);
 _wchdir(L"./tor");
 std::system("start /min tor.exe -f ../torrc.txt");
 }
